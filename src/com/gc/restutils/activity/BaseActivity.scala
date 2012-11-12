52c98da907bba0e135748c67bda43c2f57211203
Luigi Antonini <luigi.antonini84@gmail.com>
2012-11-12 18:34:11 +0100

    Miglioramenti a baseActivity

--- src/com/gc/restutils/activity/BaseActivity.scala
+++ src/com/gc/restutils/activity/BaseActivity.scala
@@ -3,15 +3,22 @@ package com.gc.restutils.activity
 import android.app.Activity
 import android.content.Intent
 import android.content.Context
+import android.content.res.Resources.NotFoundException
+import android.util.Log
+import android.view.KeyEvent
+import android.text.Spanned
+import android.text.Html
 
 trait BaseActivity extends Activity {
+
+  val TAG = classOf[BaseActivity].toString()
 
   /**
    * Go to passed activity
    *
    * @param className
    */
-  def toActivity(className: Class[Activity]): Boolean = {
+  def toActivity(className: Class[_ <: Activity]): Boolean = {
     toActivity(className, false)
   }
 
@@ -21,7 +28,7 @@ trait BaseActivity extends Activity {
    * @param className
    * @param finish
    */
-  def toActivity(className: Class[Activity], finish: Boolean): Boolean = {
+  def toActivity(className: Class[_ <: Activity], finish: Boolean): Boolean = {
     toActivity(className, finish, null)
   }
 
@@ -32,11 +39,11 @@ trait BaseActivity extends Activity {
    * @param has
    *            map of extras
    */
-  def toActivity(className: Class[Activity], extras: Map[String, Serializable]): Boolean = {
+  def toActivity(className: Class[_ <: Activity], extras: Map[String, Serializable]): Boolean = {
     toActivity(className, false, extras)
   }
 
-  def toActivity(className: Class[Activity], toFinish: Boolean, extras: Map[String, Serializable]): Boolean = {
+  def toActivity(className: Class[_ <: Activity], toFinish: Boolean, extras: Map[String, Serializable]): Boolean = {
     val intent = new Intent(this, className);
     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
@@ -54,4 +61,62 @@ trait BaseActivity extends Activity {
     return false;
   }
 
+  /**
+   * Return value of string in resurces that have this id
+   *
+   * @param name
+   * @return
+   */
+  def getString(name: String): String = {
+
+    val identifier = getResources().getIdentifier(name, "string",
+      getApplicationInfo().packageName);
+    try {
+      return getResources().getString(identifier);
+    } catch {
+      case e: NotFoundException => Log.e(TAG, "getString [" + e.getMessage() + "]"); name
+    }
+
+  }
+
+  /**
+   * Add back function to device back button
+   *
+   * @param keyCode
+   * @param event
+   * @return
+   */
+  override def onKeyDown(keyCode: Int, event: KeyEvent): Boolean = {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      toParent()
+    } else {
+      super.onKeyDown(keyCode, event);
+    }
+  }
+
+  /**
+   * Define to parent behavior , useful for back button
+   */
+  def toParent(): Boolean = {
+    finish()
+    false
+  }
+
 }
+
+object BaseActivity {
+
+  /**
+   * Return formatted Spannet text from html string and replace <strong> tag
+   * with <b> tag
+   *
+   * @param text
+   * @return
+   */
+  def fromHtml(text: String): Spanned = {
+    Html.fromHtml(text.replace("<strong>", "<b>").replace(
+      "</strong>", "</b>"))
+  }
+  
+}
+
